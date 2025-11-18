c1caf1858776a4e067afa22ef9e230426124b612
SamhithaChakilam <148128505+SamhithaChakilam@users.noreply.github.com>
2025-11-18 16:18:59 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
-const _geist = Geist({ subsets: ["latin"] });
-const _geistMono = Geist_Mono({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Ai scheme eligibility finder',
@@ -19,7 +18,7 @@ interface LayoutProps {
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <body className="font-sans antialiased">
+      <body className={`${inter.className} font-sans antialiased`}>
         {children}
         <Analytics />
       </body>
